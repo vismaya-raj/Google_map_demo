166d53ff6ba744cc798b83c8d3540213c6bf477c
Vismaya <vismayavipz999@gmail.com>
2023-02-01 23:05:10 +0530

    Gmap_app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import Form from './Form';
 
