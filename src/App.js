68082ce3295274dcf147cef591d79e8737aa3cf8
Vismaya <vismayavipz999@gmail.com>
2023-02-03 19:16:51 +0530

    Gmap

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import Form from './Form';
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+import Form from "./Form";
 
 const mapStyles = {
-  width: '50%',
-  height: '50%'
+  width: "50%",
+  height: "50%",
 };
 
 export const MapContainer = (props) => {
+  const [location, setLocation] = useState({
+    lat: "",
+    long: "",
+    isShowMap: false,
+  });
 
-  const [location,setLocation] = useState({
-    lat : '',
-    long: '',
-    isShowMap: false
-  })
-
-  const showMap = (loc) =>{
+  const showMap = (loc) => {
     console.log(loc);
     setLocation({
-      lat: loc.lat,long: loc.long,isShowMap: true
-    })
-  }
+      lat: loc.lat,
+      long: loc.long,
+      isShowMap: true,
+    });
+  };
 
-    return (
-      <>
-      <Form showMap = {showMap} name ="sujith"/> 
-      {
-        location.isShowMap  && 
-      <Map
-        google={props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
+  return (
+    <>
+      <Form showMap={showMap} name="sujith" />
+      {location.isShowMap && (
+        <Map
+          google={props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
             lat: location.lat,
-            lng: location.long
-          }
-        }
-      /> }
-      </>
-    );  
-}
+            lng: location.long,
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAzeEg1j_V3YoXjP2HIAqN2ux60xfgIM1s'
+  apiKey: "AIzaSyAzeEg1j_V3YoXjP2HIAqN2ux60xfgIM1s",
 })(MapContainer);
